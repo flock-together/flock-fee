6027825742b5b66a39b1accdb11fa6fd4c2dd8ee
Shay <hallshay@gmail.com>
2015-12-11 16:00:27 -0500

    Create login with Facebook and options to like and share.

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,37 @@
   angular.module('flockTogether', ['ngRoute'], function($routeProvider) {
       $routeProvider
       .when('/', {
-        templateUrl: 'partials/home.html'
+        templateUrl: 'partials/home.html',
+        controller: 'loginCtrl'
       })//END .when '/'
 
   })//END angular.module 'flock'
-    .controller('MainController', function(){
+  .controller('MainController', function(){
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '531876423645126',
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
 
-    });//END MainController
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  })//END MainController
+    .controller('loginCtrl', function(){
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=531876423645126";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    })
+
+
 })();//END IIFE
