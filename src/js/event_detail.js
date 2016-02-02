99ce0391561b652dba4a47559b67ac174c8bff22
Shay <hallshay@gmail.com>
2016-02-02 15:48:32 -0500

    Add attending category

---
+++ src/js/event_detail.js
@@ -0,0 +1,16 @@
+;(function(){
+
+  angular.module('flockTogether')
+    // var id = $routeParams.id;
+    // $scope.id = id;
+    .controller('detailCtrl', function($scope, $http) {
+
+    // $http.get("api/event.json")
+    //   .then(function(response){
+        console.log("Hello");
+    //     // console.log(response.data[0].event_id);
+    //     // $scope.event = response.data[0];
+    //     // $scope.event.id = response.data[0].event_id;
+    //   })//END get event.json
+    })//END eventDetail controller
+})();
