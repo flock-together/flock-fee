02e241cdc5fc6a135456907f4951c1fd606ad4cf
Shay <hallshay@gmail.com>
2016-02-03 15:01:07 -0500

    Add BASE URL var and dummy code for joinEvent.

--- src/js/event_detail.js
+++ src/js/event_detail.js
@@ -1,17 +1,25 @@
 ;(function(){
+  var BASE_URL = "api/event.json";
 
   angular.module('flockTogether')
 
-    .controller('detailCtrl', function($scope, $http, $location) {
-      var id = $location.path().split('/')[2]-1;//pulls event_id out of path for reference
-      $http.get("api/event.json")
-      .then(function(response){
-        $scope.thisEvent = response.data[id];
-        $scope.directions = $scope.thisEvent.location.split(" ").join("+");
-      })//END get event.json
+  .controller('detailCtrl', function($scope, $http, $location) {
+    var id = $location.path().split('/')[2]-1;//pulls event_id out of path and translates to index number
+    $http.get(BASE_URL)
+    .then(function(response){
+      $scope.thisEvent = response.data[id];
+      $scope.directions = $scope.thisEvent.location.split(" ").join("+");
+    });//END get event.json
 
-      $scope.joinEvent=function(){
-        console.log("Add code to put user to attending list"); 
-      }
-    })//END eventDetail controller
+    $scope.joinEvent=function(){
+      console.log("Add code to put user to attending list");
+      $http.get("https://flockapi.herokuapp.com/")
+      .then(function successCallback(response) {
+        console.log("Success");
+      },
+      function errorCallback(response) {
+        console.log(response.data);
+      })
+    }//END joinEvent
+  })//END eventDetail controller
 })();
